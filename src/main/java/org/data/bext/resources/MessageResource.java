c55ad465d64340d91d8efd54f7bd2915b0a3eb81
jalbertomr <jalbertomr@yahoo.com>
2018-03-04 20:07:51 -0600

    MessageResource getMessage(@PathParam("messageId") long id)...

--- src/main/java/org/data/bext/resources/MessageResource.java
+++ src/main/java/org/data/bext/resources/MessageResource.java
@@ -5,6 +5,7 @@ import org.data.bext.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -17,5 +18,12 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_XML)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
+    }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessage(@PathParam("messageId") long Id) {
+        return messageService.getMessage(Id);
     }
 }
