e557bc6622b557493c726c80d6080773752d6773
jalbertomr <jalbertomr@yahoo.com>
2018-03-04 23:39:22 -0600

    Se Implementa update y delete message

--- src/main/java/org/data/bext/resources/MessageResource.java
+++ src/main/java/org/data/bext/resources/MessageResource.java
@@ -8,25 +8,36 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message addMessage(Message message) {
         return messageService.addMessage(message);
     }
 
+    @PUT
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam("messageId") long id , Message message) {
+        message.setId(id);
+        return messageService.updateMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}")
+    public void deleteMessage(@PathParam("messageId") long id) {
+        messageService.removeMessage(id);
+    }
+
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long Id) {
         return messageService.getMessage(Id);
     }
